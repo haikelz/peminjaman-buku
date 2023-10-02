3753d8ba2a868abba4cc804b77b048c56f534976
haikelz <haikel.2017th@gmail.com>
2023-10-03 06:22:14 +0700

    fix: tanggal peminjaman and pengembalian

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,7 +1,9 @@
 import { addDays, format, parse } from "date-fns";
+import id from "date-fns/locale/id";
 
-export const TGL_PINJAM = format(new Date(), "yyyy-MM-dd");
+export const TGL_PINJAM = format(new Date(), "dd MMMM yyyy", { locale: id });
 export const MAKS_TGL_PENGEMBALIAN = format(
   addDays(parse(format(new Date(), "yyyy-MM-dd"), "yyyy-MM-dd", new Date()), 7),
-  "yyyy-MM-dd"
+  "dd MMMM yyyy",
+  { locale: id }
 );
